46e73979813f90c0d8de1dbdcc74485d68d978e2
admin <982584989@qq.com>
2020-02-16 14:51:32 +0800

    commit

--- src/main/java/com/lujieni/config/ScopeConfig.java
+++ src/main/java/com/lujieni/config/ScopeConfig.java
@@ -18,6 +18,11 @@ public class ScopeConfig {
      * @return
      * prototype:多实例:ioc容器启动并不会去调用方法创建对象放在容器中,而是在每次获取的时候
      * singleton:单实例(默认值):ioc容器启动会调用方法创建对象放到ioc容器中
+     *
+     * 懒加载@Lazy:
+     *        单实例bean:默认在容器启动的时候创建对象;
+     *        懒加载:容器启动不创建对象。第一次使用(获取)Bean创建对象,并初始化;
+     *
      */
     @Scope("prototype")
     @Bean
